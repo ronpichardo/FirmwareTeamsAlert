5255127f7b35f1125a70ad8018112452a5e65667
ronpichardo <ron@ronpich.com>
2020-11-12 13:32:15 -0500

    Add TeamsAlert class

---
+++ classes/Teams.py
@@ -0,0 +1,42 @@
+import requests
+import json
+from time import strftime, localtime
+
+class Teams:
+
+  def __init__(self, baseUrl):
+    self._baseUrl = baseUrl
+
+  def sendNotification(self, numOfUpdates):
+    payload = {
+      "@type": "MessageCard",
+      "@context": "http://schema.org/extensions",
+      "themeColor": "0076D7",
+      "summary": "New Crestron Firmware",
+      "sections": [{
+        "activityTitle": "Crestron Firmware",
+        "activitySubtitle": "New Firmware Available",
+        "facts": [{
+            "name": "Updates found",
+            "value": numOfUpdates
+        },{
+            "name": "Time of alert",
+            "value": strftime("%a, %d %b %Y %H:%M:%S", localtime())
+        },{
+            "name": "Status",
+            "value": "New"
+        }],
+        "markdown": True
+      }]
+    }
+
+    postMessage = requests.post(self._baseUrl, json=payload, headers={ 'Content-Type': 'application/json' })
+
+    if postMessage.status_code != 200:
+      print('Error posting message')
+      return 'Exited with message num: %s' % postMessage.text
+
+    if int(postMessage.text) != 1:
+      return 'Message exited before posting with errorCode: %s' % postMessage.text
+
+    return 'Message successfully posted!'
